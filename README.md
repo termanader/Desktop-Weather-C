f90f5478365b8745f9173fd2d9bf0639b59998fd
kruug <kruug@kruug.org>
2015-05-28 23:01:55 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Desktop Weather
+
+Currently, there is no decent weather forecasting program for the Windows desktop environment.  The Weather Channel does have one, but it's pretty large, a little flaky, and includes large advertisements.  This project is geared towards accomplishing the same goal but doing so in a smaller window with only the necessary information.
+
+This program does use the Weather Underground API and does require an API key available from [http://www.wunderground.com/weather/api/](http://www.wunderground.com/weather/api/ "Weather Underground API").
+
+## Screenshot
+
+![App in action](https://kruug.org/draconemsoft/images/desktopweather/DesktopWeather_0_8_5.png)
+
+## Installation
+
+This is a "portable" program.  No installation required.
+
+## Requirements
+
+This program requires the .Net 4.5 run-time.  Download here: [http://www.microsoft.com/en-us/download/details.aspx?id=30653](http://www.microsoft.com/en-us/download/details.aspx?id=30653)
+
+## Usage
+
+Currently, there is a constant available in the code for the API key, but the location is hard-coded.
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History/Changelog
+
+ - 2015-02-12: Stable build, first commit (v 0.1)
+ - 2015-02-17: Stable build, added manual refresh option (v 0.2)
+ - 2015-02-18: Stable build, added metric/imperial choices, started work on timed refresh (v 0.5)
+ - 2015-02-19: Stable build, changed `Imperial` to `US units`, changed WU logo to adhere to their specifications, timed refresh now working (v 0.8)
+ - 2015-03-02: Stable build, tweaked UI, separated current conditions class from forecast class, added About window (v 0.8.5)
+ - 2015-05-20: Added IP to Latitude/Longitude class/function, added some variables to start moving to new weather API (No new release)
+
+## Future/To-Do
+
+### To trigger v1.0 release
+ - Add a city/zip code chooser so that user can pick where the weather should track
+ - Fix 3-day forecast not auto-updating
+
+### Additional wish-list
+ - Add a pop-up window for forecast details
+ - Add option for more days for forecasting (currently today + 2 more, maybe expand to 5 or 10 days)
+ - Add smaller "Current Conditions" window to take up even less screen real estate
+ - Add "Keep On Top" option
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Viktor Kruug
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ desktop-weather/frmGlance.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace weather_desktop
+{
+    partial class frmGlance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGlance));
+            this.panCurrent = new System.Windows.Forms.Panel();
+            this.panToday = new System.Windows.Forms.Panel();
+            this.panTomorrow = new System.Windows.Forms.Panel();
+            this.panDayAfter = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panCurrent
+            // 
+            this.panCurrent.Location = new System.Drawing.Point(10, 10);
+            this.panCurrent.Name = "panCurrent";
+            this.panCurrent.Size = new System.Drawing.Size(100, 115);
+            this.panCurrent.TabIndex = 0;
+            // 
+            // panToday
+            // 
+            this.panToday.Location = new System.Drawing.Point(116, 10);
+            this.panToday.Name = "panToday";
+            this.panToday.Size = new System.Drawing.Size(100, 150);
+            this.panToday.TabIndex = 1;
+            // 
+            // panTomorrow
+            // 
+            this.panTomorrow.Location = new System.Drawing.Point(222, 10);
+            this.panTomorrow.Name = "panTomorrow";
+            this.panTomorrow.Size = new System.Drawing.Size(100, 150);
+            this.panTomorrow.TabIndex = 2;
+            // 
+            // panDayAfter
+            // 
+            this.panDayAfter.Location = new System.Drawing.Point(332, 10);
+            this.panDayAfter.Name = "panDayAfter";
+            this.panDayAfter.Size = new System.Drawing.Size(100, 150);
+            this.panDayAfter.TabIndex = 2;
+            // 
+            // frmGlance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 191);
+            this.Controls.Add(this.panDayAfter);
+            this.Controls.Add(this.panTomorrow);
+            this.Controls.Add(this.panToday);
+            this.Controls.Add(this.panCurrent);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmGlance";
+            this.Text = "At A Glance";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panCurrent;
+        private System.Windows.Forms.Panel panToday;
+        private System.Windows.Forms.Panel panTomorrow;
+        private System.Windows.Forms.Panel panDayAfter;
+    }
+}
+
